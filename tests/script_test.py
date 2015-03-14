b0d743bc8735f3c4db7e34043cb37c088bb213ad
Richard Bann <richard.bann@vertis.com>
2015-03-14 15:01:06 +0100

    some initial cleanup

--- tests/script_test.py
+++ tests/script_test.py
@@ -8,42 +8,22 @@ project_path = p.abspath(p.join(p.dirname(__file__), '../test_project'))
 sys.path.insert(0, project_path)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'test_project.settings'
 django.setup()
-from django.core.management import call_command
+# from django.core.management import call_command
 
+from django.contrib.auth.models import User
 from product.models import Product
-from derivative.models import Futures
+# from derivative.models import Futures
 
+# call_command('flush', interactive=False)
 
-call_command('flush', interactive=False)
-
-prod1 = Product(name='apple', price=3.141593)
+prod1 = Product(name='apple', price=1)
 prod1.save()
 
-# prod2 = Product(name='pear', price=2.718282)
-# Product.objects.bulk_create((prod1, prod2))
+alice = User.objects.get(username='Alice')
+bob = User.objects.get(username='Bob')
 
-# prod1.price = 5
-# prod1.save()
-# prod1.price = 6
-# prod1.save()
-
-# prod2 = Product.objects.get_or_create(name='pear')[0]
-
-# Product.objects.all().update(price=10)
-
-
-fut1 = Futures(name='fut1', ticker='FUT1', underlying=prod1)
-fut1.save()
-
-# Futures.objects.filter(ticker='FUT1').update(ticker='FUT_1')
-
-prod1.delete()
-
-
-# fut2 = Futures(name='fut2', ticker='FUT2', underlying=prod2)
-# fut3 = Futures(name='fut3', ticker='FUT3', underlying=prod2)
-# Futures.objects.bulk_create((fut1, fut2, fut3))
-
+prod1.maintainers.add(alice)
+prod1.save()
 
 # print 'queries num: %s' % len(django.db.connection.queries)
 # for q in django.db.connection.queries:
